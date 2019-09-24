9a9b6dfe24f953ae018024a13307372f8008ca38
sgjj <admin@loverot.cn>
2019-09-24 22:57:27 +0800

    Initial commit

---
+++ src/main/java/cn/loverot/people/PeopleApplication.java
@@ -0,0 +1,13 @@
+package cn.loverot.people;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeopleApplication.class, args);
+    }
+
+}
